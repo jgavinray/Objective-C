b51e1ac03fb82d91b6d0194a5066c1e4e775ec2d
J. Gavin Ray <jgavinray@gmail.com>
2012-05-04 07:15:08 -0700

    Coolness & While Loop

---
+++ Coolness/Coolness/Coolness/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by Gavin Ray on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i = 0;
+
+    while (i < 12) {
+        printf("%d, Jeffron is kewl\n", i);
+        i++;
+    }
+    
+    return 0;
+}
+
