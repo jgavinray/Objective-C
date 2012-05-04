5e855adc16bc6a5bae6af4e5017bf29d3996d86f
J. Gavin Ray <jgavinray@gmail.com>
2012-05-04 07:11:11 -0700

    Challenge

--- Numbers/Numbers/Numbers/main.c
+++ Numbers/Numbers/Numbers/main.c
@@ -7,16 +7,12 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, const char * argv[])
 {
 
-    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
-    printf("11 / 3.0 = %f\n", 11 / (float)3);
-    // Division, then Vision with Remainder
-    printf("11 / 3 = %d remainder of %d\n", 11 / 3, 11 % 3);
-    printf("The absolute value of -5 is %d\n", abs(-5));    
+    printf("Sin = %f", sin(1));
     
     return 0;
 }
