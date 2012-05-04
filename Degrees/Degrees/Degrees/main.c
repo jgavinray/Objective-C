fb39d83761f4fe3de2dbf4733f592ce9ca697b2e
J. Gavin Ray <jgavinray@gmail.com>
2012-05-04 06:26:45 -0700

    Finished Degrees

--- Degrees/Degrees/Degrees/main.c
+++ Degrees/Degrees/Degrees/main.c
@@ -9,6 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+static float lastTemp = 50.0;
+
 float fahrenheitFromCelsius(float cel)
 {
     float fahr = cel * 1.8 + 32.0;
@@ -21,7 +24,7 @@ int main(int argc, const char * argv[])
     float freezeInC = 0;
     float freezeInF = fahrenheitFromCelsius(freezeInC);
     printf("Water freezes at %f degrees Fahrenheit.\n", freezeInF);
-    
+    printf("The last tempature converted was %f.\n", lastTemp);
     return EXIT_SUCCESS;
 }
 
