d96b3a061395738cf514103d6a92743fce32114b
J. Gavin Ray <jgavinray@gmail.com>
2012-05-04 17:27:49 -0700

    Chapter 8 Challenge

--- Addresses/Addresses/Addresses/main.c
+++ Addresses/Addresses/Addresses/main.c
@@ -10,13 +10,9 @@
 
 int main(int argc, const char * argv[])
 {
-    int i = 17;
-    int *addressOfI = &i;
-    printf("i stores its value at %p\n", addressOfI);
-    *addressOfI = 89;
-    printf("Now i is %d\n", i);
-    printf("An int is %zu bytes.\n", sizeof(int));
-    printf("A pointer is %zu bytes.\n", sizeof(int *));
+    printf("The memory size of a float is %zu.\n", sizeof(float));
+    printf("The memory size of a short is %zu.\n", sizeof(short));
+    printf("The memory size of an unsigned short is %zu.\n", sizeof(unsigned short));
     return 0;
 }
 
