24fa510bfbbed4247095830fd9361af2a204dee1
J. Gavin Ray <jgavinray@gmail.com>
2012-05-05 11:51:16 -0700

    BMI function

--- BMICalc/BMICalc/BMICalc/main.c
+++ BMICalc/BMICalc/BMICalc/main.c
@@ -14,14 +14,17 @@ typedef struct {
     int weightInKilos;
 } Person;
 
-
+float bodyMassIndex (Person p)
+{
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
 int main(int argc, const char * argv[])
 {
     Person person;
     person.weightInKilos = 96;
     person.heightInMeters = 1.8;
-    printf("Person weighs %i kilograms.\n", person.weightInKilos);
-    printf("Person is %.2f meters tall.\n", person.heightInMeters);
+    float bmi = bodyMassIndex(person);
+    printf("person has a BMI of %.2f\n", bmi);
     return 0;
 }
 
