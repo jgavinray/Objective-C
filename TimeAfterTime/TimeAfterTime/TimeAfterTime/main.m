3ef802e039eb889956ab89ca87cd048a4df0dbe0
J. Gavin Ray <jgavinray@gmail.com>
2012-05-05 12:12:14 -0700

    Page 132

--- TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
@@ -15,6 +15,12 @@ int main(int argc, const char * argv[])
         
         NSDate *now = [NSDate date];
         NSLog(@"The new date lives at %@", now);
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        
+
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
     }
     return 0;
 }
