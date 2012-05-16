83eb836cb031ed474f97d72f16449d78b2d49bb0
J. Gavin Ray <jgavinray@gmail.com>
2012-05-16 15:58:49 -0700

    DateList

--- DateList/DateList/DateList/main.m
+++ DateList/DateList/DateList/main.m
@@ -13,6 +13,20 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+        // Create three NSDate objects
+        NSDate *now = [NSDate date];
+        NSDate *tomorrow = [now dateByAddingTimeInterval:24.0 * 60.0 * 60.0];
+        NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
+        
+        // Create an array containing all three (nil terminates the list)
+        NSArray *dateList = [NSArray arrayWithObjects:now, tomorrow, yesterday, nil];
+        
+        // How many dates are there?
+        NSLog(@"There are %lu dates", [dateList count]);
+              
+        // Print a couple
+        NSLog(@"The first date is %@", [dateList objectAtIndex:0]);
+        NSLog(@"The third date is %@", [dateList objectAtIndex:2]);
         
         
     }
