010c39a9932b285a49a26c9e9b40d92efa2614de
J. Gavin Ray <jgavinray@gmail.com>
2012-05-16 09:11:19 -0700

    Chapter 13 Challenge Solution

--- TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
@@ -13,11 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        NSTimeZone *today = [NSTimeZone systemTimeZone];
-        NSLog(@"%@ this machine is currently in daylight savings time!", today);
-//      Still need to figure out how to convert today using isDaylightSavingTime method.
-        
-        NSDate *now = [NSDate date];
+                NSDate *now = [NSDate date];
         NSLog(@"The new date lives at %@", now);
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.", seconds);
@@ -30,7 +26,20 @@ int main(int argc, const char * argv[])
         NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit
                                         inUnit:NSMonthCalendarUnit
                                        forDate:now];
+//        NSString *x = [NSString stringWithFormat:@"The best day of the month is %lu", day];
+//        NSLog(@"%@", x);
         NSLog(@"This is day %lu of the month", day);
+
+
+//      Challenge for Chapter 13
+        NSTimeZone *today = [NSTimeZone systemTimeZone];
+        BOOL isDaylight = [today isDaylightSavingTime];
+        if (isDaylight == YES) {
+            NSLog(@"It is currently daylight savings.");
+        } else {
+            NSLog(@"Is it currently not daylight savings.");
+        }
+
     }
     return 0;
 }
