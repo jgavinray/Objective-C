10ba1667a5930f08cce22b6604879a839d574ce0
J. Gavin Ray <jgavinray@gmail.com>
2012-05-16 08:05:01 -0700

    page 153

--- TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/TimeAfterTime/main.m
@@ -21,6 +21,12 @@ int main(int argc, const char * argv[])
 
         NSDate *later = [now dateByAddingTimeInterval:100000];
         NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                        inUnit:NSMonthCalendarUnit
+                                       forDate:now];
+        NSLog(@"This is day %lu of the month", day);
     }
     return 0;
 }
